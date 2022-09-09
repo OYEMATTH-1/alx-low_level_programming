2b8d22a6cecd44dd00a31ef12834f8fb69ece14a
Oyelami Matthew <oyematth@gmail.com>
2022-09-09 02:58:37 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
+#include<unitd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followd by anew line, to standardnerror.
+ *
+ * Returnn: Always 1.
+ */
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1)
+}
