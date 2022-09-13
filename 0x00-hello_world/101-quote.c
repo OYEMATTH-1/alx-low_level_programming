2ef592ab2a15052918f406ed563ae9a79269db6a
Oyelami Matthew <oyematth@gmail.com>
2022-09-13 11:35:40 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@
  * Returnn: Always 1.
  */
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1)
 }
