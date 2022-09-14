129ea3147aa7df1ca903ae86dca876d3ef8cbc10
Oyelami Matthew <oyematth@gmail.com>
2022-09-14 10:15:08 -0700

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: numbers of the tine tables
+ * @n: numbers of the time tables
  */
 void print_times_table(int n)
 {
@@ -21,24 +21,24 @@ void print_times_table(int n)
 				}
 				else if (k < 10 && j != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					_Putchar(',');
+					_Putchar(' ');
+					_Putchar(' ');
+					_Putchar(' ');
 					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
-					_Putchar(' ');
 					_putchar(' ');
+					_Putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
 				{
 					_putchar(',');
-					_putchar(' ');
+					_Putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
