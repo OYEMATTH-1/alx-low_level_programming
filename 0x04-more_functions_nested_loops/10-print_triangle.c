be66ff3381fea7f4439fbb3ebec175821e6738cb
Oyelami Matthew <oyematth@gmail.com>
2022-09-15 16:05:50 -0700

    task10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 
 	for (i = 1; i <= size; i++)
 	{
-		for(j = size - i; j > 0; j--)
+		for (j = size - i; j > 0; j--)
 			_putchar(' ');
 		for (j = i; j > 0; j--)
 			_putchar('#');
