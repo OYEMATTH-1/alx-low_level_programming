5e74020bbaf97f49962a91c186c85c000fa91021
Oyelami Matthew <oyematth@gmail.com>
2022-09-15 12:30:46 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check code
+ * Return: 0 otherwise
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	_putchar(c);
+	return (1);
+
+	c = 'a';
+	_putchar(c);
+	return (0);
+
+	-putchar('\n');
+}
