97b8d9d69d76dbf1b663476ffd5a67285c303fea
Oyelami Matthew <oyematth@gmail.com>
2022-09-15 14:43:06 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print 0-9 without 2 and 4
+ * Return - nothing
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -14,3 +14,4 @@ Write a function that multiplies two integers.
 mandatory
 
 Write a function that prints the numbers, from 0 to 9, followed by a new line
+Write a function that prints the numbers, from 0 to 9, followed by a new line
