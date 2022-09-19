faf9c44d021e6364893f35b59b307067fd13089c
Oyelami Matthew <oyematth@gmail.com>
2022-09-19 13:36:54 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - a function that prints a string, in reverse, followed by a new line
+ * print_rev - function that print string, in reverse, followed by a new line
  * @s: string input
  * Return: string in reverse manner
  */
