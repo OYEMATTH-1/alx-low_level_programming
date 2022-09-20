13049c83388bce16d86b31c3433a7ab009dbf805
Oyelami Matthew <oyematth@gmail.com>
2022-09-20 04:56:01 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,4 @@
-/*
- * File: 101-quote.c
- * Auth: Brennan D Baraban
- */
-
-#include<unitd.h>
+#include <unitd.h>
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
