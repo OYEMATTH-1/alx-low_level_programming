7496f965d96175fcaee3b4a4450ef50b7c291811
Oyelami Matthew <oyematth@gmail.com>
2022-09-20 05:09:59 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followd by anew line, to standardnerror.
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
  *
- * Returnn: Always 1.
+ * Return: Always 1.
  */
 
 int main(void)
