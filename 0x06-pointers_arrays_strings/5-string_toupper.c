7037d854686d2bbf2e0f87484fa506bab980fa96
Oyelami Matthew <oyematth@gmail.com>
2022-09-22 02:27:03 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lower case to capital lwtters
+ * @str: string to modify
+ * Return: char
+ */
+
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+
+	return (str);
+}
