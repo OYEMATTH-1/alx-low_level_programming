dabeec834c5c7433686602b6ef1d12bd4ff80623
Oyelami Matthew <oyematth@gmail.com>
2022-09-28 10:06:56 -0700

    task7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s)
 {
 	if (!*s)
 	{
-		return ());
+		return (0);
 	}
 	return (1 + _strlen_recursion(++s));
 }
@@ -31,7 +31,7 @@ int p1(char *s, int l)
 
 	if (*s == *(s + 1))
 	{
-		return (p1(s + 1, l - s));
+		return (p1(s + 1, l - 2));
 	}
 	return (0);
 }
