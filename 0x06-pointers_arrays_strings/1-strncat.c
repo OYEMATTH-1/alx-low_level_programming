e2af2a345ca2946334bf755f051038672daeb211
Oyelami Matthew <oyematth@gmail.com>
2022-10-07 12:17:48 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,14 +17,10 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; j < n; j++)
+	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
 	{
 		dest[i] = src[j];
-		i++;
 	}
-
-	if (dest[i - 1] != '\0')
-		dest[i] = '\0';
 
 	return (dest);
 }
